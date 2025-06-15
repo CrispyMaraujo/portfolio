9ac20f7ace2d1a4528f80ce3364aa2427fb2c51d
CrispimAraujo <dev.araujocris@gmail.com>
2025-06-14 22:31:07 -0300

    That's my repository to my portifolio on web

---
+++ script/script.js
@@ -0,0 +1,14 @@
+//const button = document.querySelector("button");
+//button.addEventListener("mouseover", () => {
+
+//    larguraJanela = window.innerWidth;
+//    alturaJanela = window.innerHeight;
+
+//    leftMaxButton = larguraJanela - button.offsetWidth;
+//    topMaxButton = alturaJanela - button.offsetHeight;
+
+//    leftAleatoria = Math.floor(Math.random() * leftMaxButton);
+//    topAleatoria = Math.floor(Math.random() * topMaxButton);
+//    button.style.left = leftAleatoria + 'px';
+//    button.style.top = topAleatoria + 'px';
+//});
